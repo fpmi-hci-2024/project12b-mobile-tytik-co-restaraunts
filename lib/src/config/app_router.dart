43aade470a85673bf10aa75d5f9940bc2bf16e53
Ivan Kapitanov <ivan.kapitanov@imbuesystems.com>
2024-12-17 21:15:01 +0300

    add router and locator

---
+++ lib/src/config/app_router.dart
@@ -0,0 +1,16 @@
+import 'package:auto_route/auto_route.dart';
+
+import '../feature/auth_page/presentation/sign_in_page.dart';
+import '../feature/home_page/presentation/home_page.dart';
+
+part 'app_router.gr.dart';
+
+final appRouter = AppRouter();
+
+@AutoRouterConfig()
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+    AutoRoute(page: HomeRoute.page, path: '/', initial: true),
+  ];
+}
